9038f4a481bb090e74fb9de82f5a105f54744239
zhong <zhongxuebin171980062@gmail.com>
2018-08-29 22:22:33 +0800

    SpringBoot 资源文件属性配置

---
+++ src/main/java/com/example/demo/bean/Resouroce.java
@@ -0,0 +1,39 @@
+package com.example.demo.bean;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@ConfigurationProperties(prefix = "com.sengled")
+@PropertySource(value = "classpath:config.properties")
+public class Resouroce {
+
+    private String url;
+    private String type;
+    private String name;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
