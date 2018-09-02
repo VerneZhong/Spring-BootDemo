7ccaf0b667d4f7ad4d9ef8246578f9f5079a8184
zhong <zhongxuebin171980062@gmail.com>
2018-09-02 18:35:13 +0800

    SpringBoot整合定时任务Task

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import tk.mybatis.spring.annotation.MapperScan;
 
 import javax.annotation.PostConstruct;
@@ -13,6 +14,8 @@ import javax.annotation.PreDestroy;
 @MapperScan(basePackages = "com.example.demo.mapper")
 //扫描所需的包，包含一些自用的工具类包，所在路径
 @ComponentScan(basePackages = {"com.example"})
+//开启定时任务
+@EnableScheduling
 public class DemoApplication {
 
     public static void main(String[] args) {
